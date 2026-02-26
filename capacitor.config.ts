43cc824433e622add47c0ad01424d781306be8a1
BernardoGZ <bernardogarzer@gmail.com>
2026-02-26 16:42:49 -0600

    First commit on Capacitor build. Builded android app on AStudio

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.asygurare.app',
+  appName: 'Asygurare',
+  webDir: 'public'
+};
+
+export default config;
